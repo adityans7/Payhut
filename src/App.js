014553724327d982362b992a8c27cd05cf0ea4b7
adityans7 <131303769+adityans7@users.noreply.github.com>
2023-04-21 14:31:40 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import {useState, createContext, useEffect} from 'react';
+import Header from './components/Header'
+import Main from './components/Main'
+import Login from './components/Login'
+
+const AppState = createContext();
+
+function App() {
+  const [login, setLogin] = useState(false);
+
+  return (
+  
+    <AppState.Provider value={{login, setLogin}}>
+    <div className="min-w-full h-screen">
+      { login ?
+      <div className="min-w-full min-h-full">
+        {/* Main Application */}
+        <Header />
+        <Main />
+      </div>
+      :
+      <Login />
+      }
+    </div>
+    </AppState.Provider>
+  );
+}
+
+export default App;
+export {AppState}
